69f00d43630d8cf19b6f2ccb017214bc8142fe4d
AkkySharma525 <theakshitsharma525@gmail.com>
2025-09-09 18:07:28 +0530

    first commit - starting my js journey

---
+++ basic/HelloWorld.js
@@ -0,0 +1,26 @@
+// console.log("Learning JavaScript");
+// console.log("Hello World");
+
+// a = 10;
+// console.log(a=='10');
+
+// for(i=0;i<5;i++){
+//     console.log(i);
+// }
+
+// console.log(+false);
+// console.log("-1">=0);
+// console.log("-1"<=0);
+// console.log("0"==0);
+// console.log("0"===0);
+
+// console.log("-1">0);
+
+// console.log("A"+1); //Error
+
+let arr = [1,23,4,5,6,7]
+let newArr = arr.join();
+console.log(arr)
+console.log(arr.length)
+console.log(newArr)
+console.log(newArr.length)
